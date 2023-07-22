5586ae0f8719a2089967cd3b658f357b9cfe05f9
Alexander Lozada Caviedes <99624129+Arekkasu@users.noreply.github.com>
2023-07-22 05:35:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,1 @@
-//Create a web server that can respond to requests for /comments.json with a JSON representation of the comments list and respond to requests for /comments/new with a form for submitting new comments.
-
-// Use the comments variable defined below as your data. You will need to create only two types of pages: the main comments page and the submission form page.
-
-// You can use jQuery on the client side to render the comments list and submission form to HTML. No need to do any HTML templating. We'll do templating in a later assignment.
-
-// Don't spend much time styling your pages. We just want to see that you can respond to requests for JSON and HTML.
-
-// var comments = [
-    
+//Create a web server that can respond to requests for /comments.json with a JSON representation of the comments list and to requests for /items.json with a JSON representation of the items list.
