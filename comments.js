00bc7474c7cefd0de2da88d5fa117dad7a54d2f1
Alexander Lozada Caviedes <99624129+Arekkasu@users.noreply.github.com>
2023-07-22 05:36:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,10 @@
-//Create a web server that can respond to requests for /comments.json with a JSON representation of the comments list and to requests for /items.json with a JSON representation of the items list.
+// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
+
+// Path: comments.js
+// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
+
+// Path: comments.js
+// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
+
+// Path: comments.js
+// Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance. (Try to solve this in many different ways and inspect the loading time in the browser and send many requests to see the performance differences)
